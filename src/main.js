80201abd8c6eee285994bb1db0f4310c80870ae4
Adrian Smith <agnomen@gmail.com>
2016-05-06 16:54:09 -0400

    Flipbook test.

--- src/main.js
+++ src/main.js
@@ -18,7 +18,9 @@ var MAIN = (function (gameplay, gamedraw) {
     }
     
     var batch = new BLIT.Batch("images/"),
-        testImage = batch.load("test.png");
+        testImage = batch.load("test.png"),
+        testFlip = new BLIT.Flip(batch, "test", 6, 2),
+        testFlipDraw = testFlip.setupPlayback(80, true);
     
     (function () {
         batch.commit();
@@ -39,6 +41,9 @@ var MAIN = (function (gameplay, gamedraw) {
                 if (gameplay) {
                     gameplay(now, elapsed, keyboard, pointer);
                 }
+                if (!gamedraw) {
+                    testFlip.updatePlayback(elapsed, testFlipDraw);
+                }
                 
                 keyboard.postUpdate();
                 lastTime = now;
@@ -56,6 +61,7 @@ var MAIN = (function (gameplay, gamedraw) {
                 gamedraw(canvs.width, canvas.height);
             } else if (!BLIT.isPendingBatch()) {
                 BLIT.draw(context, testImage, 100, 100, BLIT.ALIGN.Center, 0, 0, BLIT.MIRROR.Horizontal);
+                testFlip.draw(context, testFlipDraw, 200, 50, BLIT.ALIGN.Left, 0, 0, BLIT.MIRROR.Vertical);
             }
         }
 
