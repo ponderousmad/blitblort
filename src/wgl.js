46d8234fa83a98694de5391f3744e94e1fd37711
Adrian Smith <agnomen@gmail.com>
2016-06-08 15:42:25 -0400

    Texture the square.

--- src/wgl.js
+++ src/wgl.js
@@ -55,7 +55,7 @@ var WGL = (function () {
 
         return null;
     }
-    
+        
     function Viewer() {
         this.position = R3.origin();
         this.fov = 90;
@@ -110,7 +110,7 @@ var WGL = (function () {
         return shader;
     };
     
-    Room.prototype.setupShaderProgram = function (fragmentSource, vertexSource, vertexVariableName) {
+    Room.prototype.setupShaderProgram = function (fragmentSource, vertexSource, positionVariable, uvVariable) {
         var fragmentShader = this.setupShader(fragmentSource, this.gl.FRAGMENT_SHADER),
             vertexShader = this.setupShader(vertexSource, this.gl.VERTEX_SHADER);
         
@@ -130,23 +130,27 @@ var WGL = (function () {
 
         this.gl.useProgram(program);
 
-        var vertexPositionAttribute = this.gl.getAttribLocation(program, vertexVariableName);
-        this.gl.enableVertexAttribArray(vertexPositionAttribute);
+        var positionAttribute = this.gl.getAttribLocation(program, positionVariable);
+        this.gl.enableVertexAttribArray(positionAttribute);
+        
+        var uvAttribute = this.gl.getAttribLocation(program, uvVariable);
+        this.gl.enableVertexAttribArray(uvAttribute);
         
         return {
             program: program,
-            vertexPosition: vertexPositionAttribute
+            vertexPosition: positionAttribute,
+            vertexUV: uvAttribute
         };
     };
     
-    Room.prototype.setupBuffer = function (verticies, hint) {
+    Room.prototype.setupBuffer = function (data, hint) {
         if (!hint) {
             hint = this.gl.STATIC_DRAW;
         }
-        var vertexBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(verticies), hint);
-        return vertexBuffer;
+        var buffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(data), hint);
+        return buffer;
     };
     
     Room.prototype.setupUniforms = function (program, perspective, modelView) {
@@ -155,21 +159,52 @@ var WGL = (function () {
         this.gl.uniformMatrix4fv(pLocation, false, perspective.m);
         this.gl.uniformMatrix4fv(mvLocation, false, modelView.m);
     };
+
+    Room.prototype.loadTexture = function(batch, resource) {
+        var gl = this.gl;
+        
+        function setupTexture(image, texture) {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+            gl.generateMipmap(gl.TEXTURE_2D);
+            gl.bindTexture(gl.TEXTURE_2D, null);
+        }
+        
+        var texture = gl.createTexture();
+        batch.load(resource, function(image) { setupTexture(image, texture); });
+        return texture;
+    };
     
     Room.prototype.drawTest = function () {
         if (!this.testSetup) {
             var vertexSource = document.getElementById("vertex-test").innerHTML,
                 fragmentSource = document.getElementById("fragment-test").innerHTML;
-            this.testSetup = this.setupShaderProgram(fragmentSource, vertexSource, "aVertexPosition");
+            this.testSetup = this.setupShaderProgram(fragmentSource, vertexSource, "aPos", "aUV");
             
             var vertices = [
-                 1.0,  1.0, 0.0,
-                -1.0,  1.0, 0.0,
-                 1.0, -1.0, 0.0,
-                -1.0, -1.0, 0.0
-            ];
+                    -1.0, -1.0, 0.0,
+                    -1.0,  1.0, 0.0,
+                     1.0, -1.0, 0.0,
+                     1.0,  1.0, 0.0,
+                ],
+                uvs = [
+                    0.0,  1.0,
+                    0.0,  0.0,
+                    1.0,  1.0,
+                    1.0,  0.0
+                ];
             this.testSetup.square = this.setupBuffer(vertices);
+            this.testSetup.squareUVs = this.setupBuffer(uvs);
             this.gl.disable(this.gl.CULL_FACE);
+            
+            this.testSetup.batch = new BLIT.Batch("images/");
+            this.testSetup.squareTexture = this.loadTexture(this.testSetup.batch, "uv.png");
+            this.testSetup.batch.commit();
+        }
+        if (!this.testSetup.batch.loaded) {
+            return;
         }
         this.viewer.position.set(0, 0, 2);
         var perspective = this.viewer.perspective(this.aspect()),
@@ -177,8 +212,14 @@ var WGL = (function () {
         
         view.translate(R3.toOrigin(this.viewer.position));
         
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.testSetup.squareTexture);
+        this.gl.uniform1i(this.gl.getUniformLocation(this.testSetup.program, "uSampler"), 0);
+        
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.testSetup.square);
         this.gl.vertexAttribPointer(this.testSetup.vertexPosition, 3, this.gl.FLOAT, false, 0, 0);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.testSetup.squareUVs);
+        this.gl.vertexAttribPointer(this.testSetup.vertexUV, 2, this.gl.FLOAT, false, 0, 0);
         this.setupUniforms(this.testSetup.program, perspective, view);
         this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
     };
