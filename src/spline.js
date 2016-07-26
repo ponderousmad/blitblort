305af20d88f279e793bfa58803b252a7857c271a
Adrian Smith <agnomen@gmail.com>
2016-07-25 20:45:49 -0400

    Basic splines!

--- src/spline.js
+++ src/spline.js
@@ -2,6 +2,25 @@ var SPLINE = (function () {
     "use strict";
 
     function S() {
+        this.points =  [
+            new R2.V(10, 10),
+            new R2.V(10, 20),
+            new R2.V(10, 20),
+            new R2.V(10, 20)
+        ];
+    }
+
+    S.prototype.evaluate = function (p, points) {
+        var results = [];
+        points = points || this.points;
+        for (var i = 1; i < points.length; ++i) {
+            var prime = points[i-1].interpolate(points[i], p);
+            results.push(prime);
+        }
+        if (results.length > 1) {
+            results = this.evaluate(p, results);
+        }
+        return results;
     }
 
     return {
