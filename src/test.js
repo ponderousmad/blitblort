4a14cb9b02b966f6cd3344642a7591e5fdbe9bbc
Adrian Smith <agnomen@gmail.com>
2016-08-27 19:10:22 -0400

    Show passed test counts.

--- src/test.js
+++ src/test.js
@@ -35,24 +35,31 @@ var TEST = (function () {
     TEST.inList = function (list, item) { return TEST.isTrue(TEST.contains(list, item)); };
     TEST.tolEquals = function (a, b, tolerance) { TEST.isTrue(Math.abs(a-b) < (tolerance ? tolerance : 1e-6)); };
     
-    TEST.run  = function (name, tests) {
-        console.log("Running " + name + " Tests");
+    TEST.run = function (name, tests) {
         if (!Array.isArray(tests)) {
             tests = [tests];
         }
+        if (tests.length == 0) {
+            return;
+        }
+        console.log("Running " + name + " Tests");
+        var passed = 0;
         for (var t = 0; t < tests.length; ++t) {
             var test = tests[t];
             if (catchExceptions) {
                 try {
                     test();
+                    passed += 1;
                 } catch(e) {
                     console.log("Failed " + test.name + ":");
                     console.log(e);
                 }
             } else {
                 test();
+                passed += 1;
             }
         }
+        console.log(passed + " tests passed.");
     };
     
     TEST.INCLUDE_SLOW = false;
