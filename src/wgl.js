39099e6095bd888de4fd9f6c2a1a29d25c830cbc
Adrian Smith <agnomen@gmail.com>
2017-04-29 19:51:58 -0400

    Fix bounds check for atlas add

--- src/wgl.js
+++ src/wgl.js
@@ -764,7 +764,7 @@ var WGL = (function () {
 
         ++this.placed;
         this.xOffset += this.width;
-        if (this.xOffset > this.size) {
+        if (this.xOffset >= this.size) {
             this.xOffset = 0;
             this.yOffset += this.height;
         }
