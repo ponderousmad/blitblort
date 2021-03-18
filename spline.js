a22940e4b35438eacc653f8b5c0012b2b07dc237
Adrian Smith <agnomen@gmail.com>
2021-03-18 17:22:04 -0400

    Splines now always share end point with next segment and can optionally be closed

--- spline.js
+++ spline.js
@@ -9,30 +9,48 @@ var SPLINE = (function () {
         this.points.push(p);
     };
 
-    BezierCurve.prototype.evaluate = function (p, points) {
+    BezierCurve.prototype.start = function () {
+        return this.points[0];
+    };
+
+    BezierCurve.prototype.end = function () {
+        return this.points[this.points.length - 1];
+    };
+
+    function interpolate(a, b, parameter, results) {
+        results.push(a.interpolate(b, parameter));
+    }
+
+    BezierCurve.prototype.evaluate = function (parameter, points, prior, post) {
         var results = [];
         points = points || this.points;
+        if (prior) {
+            interpolate(prior, this.start(), parameter, results);
+        }
         for (var i = 1; i < points.length; ++i) {
-            var prime = points[i-1].interpolate(points[i], p);
-            results.push(prime);
+            interpolate(points[i-1], points[i], parameter, results);
+        }
+        if (post) {
+            interpolate(this.end(), post, parameter, results);
         }
         if (results.length === 1) {
             return results;
         }
-        return this.evaluate(p, results);
+        return this.evaluate(parameter, results);
     };
 
-    BezierCurve.prototype.build = function (count, out) {
+    BezierCurve.prototype.build = function (count, out, prior, post) {
         var points = out === undefined ? [] : out,
             stepSize = 1 / count;
         for (var c = 0; c <= count; ++c) {
-            points.push(this.evaluate(c * stepSize)[0]);
+            points.push(this.evaluate(c * stepSize, undefined, prior, post)[0]);
         }
         return points;
     };
 
-    function Spline() {
+    function Spline(closed) {
         this.segments = [];
+        this.closed = closed;
     }
 
     Spline.prototype.addSegment = function (segment) {
@@ -42,9 +60,15 @@ var SPLINE = (function () {
     Spline.prototype.build = function (segmentCount, out) {
         var points = out === undefined ? [] : out;
         for (var s = 0; s < this.segments.length; ++s) {
-            this.segments[s].build(segmentCount, points);
+            var prior = s > 0 ? this.segments[s-1].end() : null;
+            var post = this.closed && s === this.segments.length - 1 ? this.segments[0].start() : null;
+            this.segments[s].build(segmentCount, points, prior, post);
         }
         return points;
+    };
+
+    Spline.prototype.isClosed = function () {
+        return this.closed;
     };
 
     return {
