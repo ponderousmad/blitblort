e4b5c0fe337ebe90abbcfaae15a2a6be291c27b1
Adrian Smith <agnomen@gmail.com>
2021-03-21 16:36:46 -0400

    Revamp keyboard IO for robustness and portability

--- io.js
+++ io.js
@@ -1,71 +1,216 @@
 var IO = (function (TICK, BLORT) {
     "use strict";
 
-    var KEYS = {
-        Up : 38,
-        Down : 40,
-        Left : 37,
-        Right : 39,
-        Space : 32,
-        Escape : 27,
-        Plus : 187,
-        Minus : 189,
-        Backspace : 8,
-        Delete : 46,
-        LT : 188,
-        GT : 190
+    const KEYS = {
+        Up : "ArrowUp",
+        Down : "ArrowDown",
+        Left : "ArrowLeft",
+        Right : "ArrowRight",
+        Space : "Space",
+        Escape : "Escape",
+        Minus : "Minus",
+        Backspace : "Backspace",
+        Delete : "Delete",
+        LT : "Comma",
+        GT : "Period"
     };
+
+    const SHIFT_KEYS = ["ShiftLeft", "ShiftRight"];
+
+    // There's an odd behaviour where if two shift keys are pressed, 
+    // you won't get told when the first is lifted, only once both are.
+    // Can't fix properly, but can atleast repair when detected.
+    function shiftFix(keyboard, isShift) {
+        if (keyboard.shiftCount > 0 && !isShift) {
+            keyboard.shiftCount = 0;
+            SHIFT_KEYS.forEach(key=> {
+                if (keyboard.log && keyboard.pressed[key]) {
+                    console.log(`Fixing stuck ${key}`);
+                }
+                delete keyboard.pressed[key];
+            });
+        }
+    }
 
     function Keyboard(element, capture) {
         this.pressed = {};
         this.lastPressed = {};
+        this.shiftCount = 0;
+        this.ctrlCount = 0;
+        this.altCount = 0;
+        this.log = true;
         var self = this;
+
+        // When regaining focus, basically assume all bets are off.
+        document.addEventListener('visibilitychange', () => {
+            if (!document.hidden) {
+                if (self.log) {
+                    console.log("Resetting keyboard state on shown.");
+                }
+                self.pressed = {};
+                self.shiftCount = 0;
+                self.ctrlCount = 0;
+                self.altCount = 0;
+            }
+        });
+
+        document.addEventListener('focus', () => {
+            if (self.log) {
+                console.log("Resetting keyboard state on focus.");
+            }
+            self.pressed = {};
+            self.shiftCount = 0;
+            self.ctrlCount = 0;
+            self.altCount = 0;
+        });
 
         if (element) {
             element.addEventListener("keydown", function (e) {
                 e = e || window.event;
-                self.pressed[e.keyCode] = TICK.now();
+                var prevPressed = self.pressed[e.code];
+                var eventInfo = {
+                    time: TICK.now(),
+                    shift: e.shiftKey,
+                    ctrl: e.ctrlKey,
+                    alt: e.altKey
+                };
+                if (prevPressed) {
+                    prevPressed.repeats.push(eventInfo);
+                } else {
+                    switch (e.code) {
+                        case "ShiftLeft":
+                        case "ShiftRight":
+                            ++self.shiftCount;
+                            break;
+
+                        case "ControlLeft":
+                        case "ControlRight":
+                            ++self.ctrlCount;
+                            break;
+
+                        case "AltLeft":
+                        case "AltRight":
+                            ++self.altCount;
+                            break;
+                    }
+                    eventInfo.repeats = [];
+                    self.pressed[e.code] = eventInfo;
+                }
+                shiftFix(self, e.shiftKey);
                 if (capture) {
                     e.preventDefault();
+                }
+                if (self.log) {
+                    console.log("keydown", e.code, self.status());
                 }
             }, capture);
 
             element.addEventListener("keyup", function (e) {
                 e = e || window.event;
-                delete self.pressed[e.keyCode];
+                if (self.pressed[e.code]) {
+                    switch (e.code) {
+                        case "ShiftLeft":
+                        case "ShiftRight":
+                            --self.shiftCount;
+                            break;
+
+                        case "ControlLeft":
+                        case "ControlRight":
+                            --self.ctrlCount;
+                            break;
+
+                        case "AltLeft":
+                        case "AltRight":
+                            --self.altCount;
+                            break;
+                    }
+                    delete self.pressed[e.code];
+                } else if(self.log) {
+                    console.log("Unpaired keyup!", e.code, self.status());
+                }
+                shiftFix(self, e.shiftKey);
                 if (capture) {
                     e.preventDefault();
+                }
+                if (self.log) {
+                    console.log("keyup", e.code, self.status());
                 }
             }, capture);
         }
     }
 
-    Keyboard.prototype.isKeyDown = function (keyCode) {
-        return this.pressed[keyCode] ? true : false;
+    Keyboard.prototype.status = function() {
+        var s = {
+            shiftCount: this.shiftCount,
+            ctrlCount: this.ctrlCount,
+            altCount: this.altCount,
+            pressed: {},
+            lastPressed: []
+        };
+        Object.entries(this.pressed).forEach(e=>{
+            const info = e[1];
+            s.pressed[e[0]] = {
+                time:info.time,
+                shift:info.shift,
+                ctrl:info.ctrl,
+                alt:info.alt,
+                repeatCount:info.repeats.length
+            };
+        });
+        Object.keys(this.lastPressed).forEach(key=>{ s.lastPressed.push(key); });
+        return s;
     };
 
-    Keyboard.prototype.wasKeyPressed = function (keyCode) {
-        return this.pressed[keyCode] ? !this.lastPressed[keyCode] : false;
+    Keyboard.prototype.isKeyDown = function (keyName) {
+        return this.pressed[keyName] ? true : false;
+    };
+
+    Keyboard.prototype.wasKeyPressed = function (keyName, modifiers) {
+        var keyPressed = this.pressed[keyName];
+        if (keyPressed && !this.lastPressed[keyName]) {
+            if (modifiers) {
+                if (keyPressed.shift !== modifiers.shift && modifiers.hasOwnProperty("shift")) {
+                    return false;
+                }
+
+                if (keyPressed.ctrl !== modifiers.ctrl && modifiers.hasOwnProperty("ctrl")) {
+                    return false;
+                }
+
+                if (keyPressed.alt !== modifiers.alt && modifiers.hasOwnProperty("alt")) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     };
 
     Keyboard.prototype.isShiftDown = function () {
-        return this.isKeyDown(16);
+        return this.shiftCount > 0;
     };
 
     Keyboard.prototype.isCtrlDown = function () {
-        return this.isKeyDown(17);
+        return this.ctrlCount > 0;
     };
 
     Keyboard.prototype.isAltDown = function () {
-        return this.isKeyDown(18);
+        return this.altCount > 0;
     };
+
+    function makeKeyName(ascii) {
+        if ("0" <= ascii && ascii <= "9") {
+            return("Digit" + ascii);
+        }
+        return("Key" + ascii.toUpperCase());
+    }
 
     Keyboard.prototype.isAsciiDown = function (ascii) {
-        return this.isKeyDown(ascii.charCodeAt());
+        return this.isKeyDown(makeKeyName(ascii));
     };
 
-    Keyboard.prototype.wasAsciiPressed = function (ascii) {
-        return this.wasKeyPressed(ascii.charCodeAt());
+    Keyboard.prototype.wasAsciiPressed = function (ascii, modifiers) {
+        return this.wasKeyPressed(makeKeyName(ascii), modifiers);
     };
 
     Keyboard.prototype.keysDown = function () {
@@ -87,8 +232,8 @@ var IO = (function (TICK, BLORT) {
         }
     };
 
-    Keyboard.prototype.keyTime = function (keyCode) {
-        return this.pressed[keyCode];
+    Keyboard.prototype.keyTime = function (keyName) {
+        return this.pressed[keyName].time;
     };
 
     function Mouse(element, preventDefault) {
@@ -349,6 +494,10 @@ var IO = (function (TICK, BLORT) {
 
     return {
         KEYS: KEYS,
+        UNMODIFIED: {shift: false, ctrl: false, alt: false},
+        SHIFT:      {shift: true,  ctrl: false, alt: false},
+        CTRL:       {shift: false, ctrl: true,  alt: false},
+        ALT:        {shift: false, ctrl: false, alt: true},
         Keyboard: Keyboard,
         Mouse: Mouse,
         Touch: Touch,
