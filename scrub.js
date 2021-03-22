7d398c755a42d8f8935882e80d9ab302f7d8a9b0
Adrian Smith <agnomen@gmail.com>
2021-03-22 16:30:35 -0400

    First iteration of 'Scrub' module.

---
+++ scrub.js
@@ -0,0 +1,52 @@
+var SCRUB = (function (TICK) {
+    "use strict";
+
+    function Sequence(keyName, tagNames, timeFunction) {
+        this.entries =  [];
+        this.timeFunction = timeFunction || Date.now;
+        this.keyNames = [keyName || "DateTime"];
+        if (tagNames) {
+            if (Array.isArray(tagNames)) {
+                this.keyNames.push(...tagNames);
+            } else if(typeof tagNames === "object") {
+                this.keyNames.push(...tagNames.keys());
+            }
+        }
+        this.setStart(this.timeFunction());
+    }
+
+    Sequence.prototype.makeEntry = function (key, value, tags) {
+        var entry = { value: value };
+        entry[this.keyNames[0]] = key;
+        if (tags) {
+            if (Array.isArray(tags)) {
+                for (var i = 1; i < this.keyNames.length && i <= tags.length; ++i) {
+                    entry[this.keyName[i]] = tags[i];
+                }
+            } else if(this.keyNames.length > 1) {
+                entry[this.keyNames[1]] = tags;
+            }
+        }
+        return entry;
+    };
+
+    Sequence.prototype.push = function (key, value, tags) {
+        this.entries.push(this.makeEntry(key, value, tags));
+    };
+
+    Sequence.prototype.snapshot = function (value) {
+        this.push(this.timeFunction(), value);
+    };
+
+    Sequence.prototype.setStart = function (startValue, startTags) {
+        this.start = makeEntry(startValue, undefined, startTags);
+    };
+
+    Sequence.prototype.setEnd = function (endValue, endTags) {
+        this.end = makeEntry(endValue, undefined, endTags);
+    };
+
+    return {
+        Sequence : Sequence
+    };
+})();
