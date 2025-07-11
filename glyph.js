46a3fca39653fba6130983b4ca9f9f2c0ad0aedb
ponderousmad <agnomen@gmail.com>
2025-07-11 19:02:48 -0400

    Starting actual font support
    Rewrite to use classes
    Implement basic serialization

--- glyph.js
+++ glyph.js
@@ -1,27 +1,148 @@
-var GLYPH = (function (SPLINE) {
+var GLYPH = (function () {
     "use strict";
 
-    function Glyph(symbol, splines) {
-        this.symbol = symbol;
-        this.spline = splines || [];
+    function codePointForCharacter(character) {
+        if (typeof character === "string" && character.length === 1) {
+            return character.codePointAt(0);
+        }
+        throw new TypeError("Character must be a single character string");
     }
 
-    Glyph.prototype.getSymbol = function() { return this.symbol; };
-    Glyph.prototype.getSplines = function() { return this.splines; };
+    class Glyph extends Object {
+        constructor(codePoint, splines) {
+            super();
+            if (!Number.isInteger(codePoint) || codePoint < 0) {
+                throw new TypeError("Must pass codepoint as non-negative integer");
+            }
+            this.codePoint = codePoint;
+            this.splines = splines || [];
+            if (!Array.isArray(this.splines)) {
+                throw new TypeError("If splines are provided they must be in an array");
+            }
+        }
 
-    function Font() {
-        this.glyphs = {};
+        getCodePoint() { return this.codePoint; }
+        getSplines() { return this.splines; }
+        getSymbol() { return String.fromCodePoint(this.codePoint); }
     }
 
-    Font.prototype.newGlyph = function (symbol, splines) {
-        this.glyphs[symbol] = spline;
-    };
+    function loadPoint(pointData) {
+        if(typeof(pointData.z) === 'undefined') {
+            return new R2.V(pointData.x, pointData.y);
+        }
+        return new R3.V(pointData.x, pointData.y, pointData.z);
+    }
 
-    Font.prototype.getGlyph = function (symbol) {
-        return this.glyphs[symbol];
-    };
+    class Font extends Object {
+        glyphs = [];
 
-    Font.prototype.load = function (data) {
-        
+        constructor(name) {
+            super();
+            this.name = name || "Unnamed Font";
+        }
+
+        newGlyph(codePoint, splines) {
+            this.glyphs[codePoint] = new Glyph(codePoint, splines);
+            return this.glyphs[codePoint];
+        }
+
+        glyphForCodepoint(codePoint) {
+            if (!Number.isInteger(codePoint) || codePoint < 0) {
+                throw new TypeError("Must pass codepoint as non-negative integer");
+            }
+            if (this.glyphs.length > codePoint) {
+                return this.glyphs[codePoint];
+            }
+            return null;
+        }
+
+        glyphForCharacter(character) {
+            return this.glyphForCodepoint(codePointForCharacter(character));
+        }
+
+        asJSONString() {
+            let glyphData = [];
+            for (let glyph of this) {
+                let splines = glyph.getSplines(),
+                    splineData = [];
+
+                for (let p = 0; p < splines.length; ++p) {
+                    splineData.push(splines[p].getData());
+                }
+                glyphData.push({
+                    codePoint: glyph.getCodePoint(),
+                    symbol : glyph.getSymbol(),
+                    splines : splineData
+                });
+            }
+            return JSON.stringify({name:this.name, glyphs:glyphData}, null, 4);
+        }
+
+        loadFromJSON(jsonString) {
+            let data = JSON.parse(jsonString);
+
+            let newGlpyhs = [];
+            for (let g = 0; g < data.glyphs.length; ++g) {
+                let glyph = data.glyphs[g],
+                    codePoint = glyph.codePoint,
+                    splines = [];
+                for (let p = 0; p < glyph.splines.length; ++p) {
+                    let splineData = glyph.splines[p],
+                        spline = new SPLINE.Path(splineData.closed);
+                    for (let s = 0; s < splineData.segments.length; ++s) {
+                        let segmentData = splineData.segments[s];
+                        if (segmentData.type == "BezierCurve") {
+                            let segment = new SPLINE.BezierCurve();
+                            spline.addSegment(segment);
+                            for (let i = 0; i < segmentData.points.length; ++i) {
+                                segment.addPoint(loadPoint(segmentData.points[i]));
+                            }
+                        } else {
+                            throw Error("Missing required spline type");
+                        }
+                    }
+                    splines.push(spline);
+                }
+                newGlpyhs[codePoint] = new GLYPH.Glyph(codePoint, splines);
+            }
+
+            this.name = data.name;
+            this.glyphs = newGlpyhs;
+        }
+
+        [Symbol.iterator]() {
+            function nextValidCodePoint (glyphs, codePoint) {
+                while(codePoint < glyphs.length && typeof glyphs[codePoint] == 'undefined') {
+                    ++codePoint;
+                }
+                return codePoint;
+            }
+
+            const glyphs = this.glyphs;
+            let codePoint = nextValidCodePoint(glyphs, 0);
+
+            return {
+                next() {
+                    const current = codePoint;
+                    if(current < glyphs.length) {
+                        codePoint = nextValidCodePoint(glyphs, codePoint + 1);
+                        return {
+                            value: glyphs[current],
+                            done: codePoint < glyphs.length
+                        };
+                    }
+                    return {
+                        value: undefined,
+                        done: true
+                    };
+                }
+            };
+        }
+    }
+
+    return {
+        codePointForCharacter: codePointForCharacter,
+        Glyph: Glyph,
+        Font: Font
     };
 })();
